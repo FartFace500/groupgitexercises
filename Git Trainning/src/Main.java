2f4cebe5d8d53a9227c6f4d3a85ec6f100b878f8
knogleknuserHome <knogleknuser@hotmail.com>
2024-02-01 16:49:26 +0100

    Done a little coding

--- Git Trainning/src/Main.java
+++ Git Trainning/src/Main.java
@@ -1,5 +1,26 @@
+import java.util.Random;
+
 public class Main {
     public static void main( String[] args ) {
-        System.out.println( "Hello world!" );
+        
+        for ( int i = 0; i < 1000; i++ ) {
+            Random random = new Random();
+            
+            int iInput = random.nextInt( 100 ) + 1;
+            
+            System.out.println( "Input = " + iInput );
+            
+            if ( iInput % 3 == 0 && iInput % 5 == 0 ) {
+                System.out.println( "FizzBuzz" );
+            } else if ( iInput % 3 == 0 ) {
+                System.out.println( "Fizz" );
+            } else if ( iInput % 5 == 0 ) {
+                System.out.println( "Buzz" );
+            } else {
+                System.out.println( iInput );
+            }
+            System.out.println();
+        }
+        
     }
 }

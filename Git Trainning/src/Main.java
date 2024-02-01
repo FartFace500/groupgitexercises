201bdabfc0dde8b252891a9f636b5e73117da4ad
Loudi <loudimail@gmail.com>
2024-02-01 17:03:34 +0100

    fixed exercises 1 code in main

--- Git Trainning/src/Main.java
+++ Git Trainning/src/Main.java
@@ -1,16 +1,20 @@
+import java.util.Random;
+
 public class Main {
     public static void main( String[] args ) {
+        Random random = new Random();
         for (int i = 0; i < 100; i++) {
-        if ((int)(Math.random() * 100) % 3 == 0){
-        System.out.println((int)(Math.random() * 100)+" Fizz");
-        }
-        if ((int)(Math.random() * 100) % 5 == 0){
-            System.out.println((int)(Math.random() * 100)+" Fuzz");
-        }
-        if ((int)(Math.random() * 100) % 5 == 0 && (int)(Math.random() * 100) % 3 == 0){
-            System.out.println((int)(Math.random() * 100)+" FizzFuzz");
-        }else {
-        System.out.println((int)(Math.random() * 100));
+            int roll = random.nextInt(100)+1;
+        if (roll % 5 == 0 && roll % 3 == 0){
+            System.out.println(roll+" FizzFuzz");
+        } else
+        if (roll % 3 == 0){
+        System.out.println(roll+" Fizz");
+        } else
+        if (roll % 5 == 0){
+            System.out.println(roll+" Fuzz");
+        } else {
+        System.out.println(roll);
         }
         }
     }

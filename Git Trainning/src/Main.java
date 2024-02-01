247f9d27a4631077efd3adf04cc9ec79c3820eb0
knogleknuserHome <knogleknuser@hotmail.com>
2024-02-01 17:41:02 +0100

    Let us never use the stash again!

--- Git Trainning/src/Main.java
+++ Git Trainning/src/Main.java
@@ -2,33 +2,37 @@ import java.util.Random;
 
 public class Main {
     public static void main( String[] args ) {
-        int maxNumber = 200;
-        {
-            Random random = new Random();
-            
-            for ( int i = 0; i < 100; i++ ) {
-                int roll = random.nextInt( maxNumber ) + 1;
-                if ( roll % 5 == 0 && roll % 3 == 0 ) {
-                    System.out.println( roll + " FizzFuzz" );
-                } else if ( roll % 3 == 0 ) {
-                    System.out.println( roll + " Fizz" );
-                } else if ( roll % 5 == 0 ) {
-                    System.out.println( roll + " Fuzz" );
-                } else {
-                    System.out.println( roll );
-                }
-            }
-        }
+        printNumbers( 0,200 );
+        
+    }
+    public static void printNumbers(int start, int end) {
+//        int maxNumber = 200;
+//        {
+//            Random random = new Random();
+//
+//            for ( int i = 0; i < 100; i++ ) {
+//                int roll = random.nextInt( maxNumber ) + 1;
+//                if ( roll % 5 == 0 && roll % 3 == 0 ) {
+//                    System.out.println( roll + " FizzFuzz" );
+//                } else if ( roll % 3 == 0 ) {
+//                    System.out.println( roll + " Fizz" );
+//                } else if ( roll % 5 == 0 ) {
+//                    System.out.println( roll + " Fuzz" );
+//                } else {
+//                    System.out.println( roll );
+//                }
+//            }
+//        }
         
         {
-            Random random = new Random();
+//            Random random = new Random();
             int iInput;
             
-            for ( int i = 0; i < 1000; i++ ) {
+            for ( int i = start; i < end+1; i++ ) {
                 
-                iInput = random.nextInt( maxNumber ) + 1;
+                iInput = i;
                 
-                System.out.println( "Input = " + iInput );
+//                System.out.println( "Input = " + iInput );
                 
                 if ( iInput % 3 == 0 && iInput % 5 == 0 ) {
                     System.out.println( "FizzBuzz" );
@@ -43,6 +47,9 @@ public class Main {
             }
             
         }
-        
     }
+    
+    
+    
+    
 }

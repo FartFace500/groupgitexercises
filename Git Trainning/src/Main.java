25462d2faf56d15379db1314c9be61e5b5554dc6
Loudi <loudimail@gmail.com>
2024-02-01 16:54:49 +0100

    Made exercises 1 code in main

--- Git Trainning/src/Main.java
+++ Git Trainning/src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main( String[] args ) {
-        System.out.println( "Hello world!" );
+        for (int i = 0; i < 100; i++) {
+        if ((int)(Math.random() * 100) % 3 == 0){
+        System.out.println((int)(Math.random() * 100)+" Fizz");
+        }
+        if ((int)(Math.random() * 100) % 5 == 0){
+            System.out.println((int)(Math.random() * 100)+" Fuzz");
+        }
+        if ((int)(Math.random() * 100) % 5 == 0 && (int)(Math.random() * 100) % 3 == 0){
+            System.out.println((int)(Math.random() * 100)+" FizzFuzz");
+        }else {
+        System.out.println((int)(Math.random() * 100));
+        }
+        }
     }
 }
